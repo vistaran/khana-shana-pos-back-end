6465735665ed83d85c164977d8e93d47d0573cce
Vandanaba17 <vandanaba.gohil17@gmail.com>
2022-03-10 16:20:52 +0530

    category fixed

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,64 +9,60 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+class CategoryController extends Controller {
 
-class CategoryController extends Controller
-{
-
-    public function show()
-    {
+    public function show() {
         try {
-            $category = Category::select('category.*', DB::raw('COUNT(category_product.category_id) as number_of_products'))
-                ->join('category_product', 'category_product.category_id', '=', 'category.id')
-                ->groupBy('category.id')
-                ->orderBy('id', 'DESC')
-                ->paginate(10);
-            return response()->json([
-                "Category" => $category,
+            $category = Category::select( 'category.*', DB::raw( 'COUNT(category_product.category_id) as number_of_products' ) )
+            ->join( 'category_product', 'category_product.category_id', '=', 'category.id' )
+            ->groupBy( 'category.id' )
+            ->orderBy( 'id' )
+            ->paginate( 10 );
+            return response()->json( [
+                'Category' => $category,
 
-
-            ]);
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            return response()->json(['error' => $e->getMessage() . ' ' . $e->getLine()]);
+            ] );
+        } catch ( Exception $e ) {
+            Log::error( $e->getMessage() );
+            return response()->json( [ 'error' => $e->getMessage() . ' ' . $e->getLine() ] );
         }
     }
-    public function edit($id, Request $request)
-    {
+
+    public function edit( $id, Request $request ) {
         try {
-            $credential = $request->only(['name', 'visible_in_menu', 'position', 'display_mode', 'description', 'image', 'category_logo', 'parent_category', 'attri', 'meta_title', 'slug', 'meta_description', 'meta_keyword', 'status']);
-            Category::where('id', $id)
-                ->update([
-                    'name' => $request->name,
-                    'visible_in_menu' => $request->visible_in_menu,
-                    'position' => $request->position,
-                    'display_mode' => $request->display_mode,
-                    'decription' => $request->decription,
-                    'image' => $request->image,
-                    'category_logo' => $request->category_logo,
-                    'parent_category' => $request->parent_category,
-                    'attributes' => $request->attri,
-                    'meta_title' => $request->meta_title,
-                    'slug' => $request->slug,
-                    'meta_description' => $request->meta_description,
-                    'meta_keyword' => $request->meta_keyword,
-                    'status' => $request->status,
-                ]);
-            return response()->json([
+            $credential = $request->only( [ 'name', 'visible_in_menu', 'position', 'display_mode', 'description', 'image', 'category_logo', 'parent_category', 'attri', 'meta_title', 'slug', 'meta_description', 'meta_keyword', 'status' ] );
+            Category::where( 'id', $id )
+            ->update( [
+                'name' => $request->name,
+                'visible_in_menu' => $request->visible_in_menu,
+                'position' => $request->position,
+                'display_mode' => $request->display_mode,
+                'decription' => $request->decription,
+                'image' => $request->image,
+                'category_logo' => $request->category_logo,
+                'parent_category' => $request->parent_category,
+                'attributes' => $request->attri,
+                'meta_title' => $request->meta_title,
+                'slug' => $request->slug,
+                'meta_description' => $request->meta_description,
+                'meta_keyword' => $request->meta_keyword,
+                'status' => $request->status,
+            ] );
+            return response()->json( [
                 'Update Message' => 'Successfully Updated !',
-            ]);
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            return response()->json(['error' => $e->getMessage() . ' ' . $e->getLine()]);
+            ] );
+        } catch ( Exception $e ) {
+            Log::error( $e->getMessage() );
+            return response()->json( [ 'error' => $e->getMessage() . ' ' . $e->getLine() ] );
         }
     }
-    public function add(Request $request)
-    {
+
+    public function add( Request $request ) {
 
         try {
-            $credential = $request->only([
+            $credential = $request->only( [
                 'name', 'visible_in_menu', 'position', 'display_mode', 'description', 'image', 'category_logo', 'parent_category', 'attri', 'meta_title', 'slug', 'meta_description', 'meta_keyword', 'status'
-            ]);
+            ] );
             $cp = new CategoryProduct();
             $category = new Category();
 
@@ -85,80 +81,79 @@ class CategoryController extends Controller
             $category->meta_keyword = $request->meta_keyword;
             $category->status = $request->status;
             $category->save();
-            $category_id = $category->where('name', $request->name)->first()->id;
+            $category_id = $category->where( 'name', $request->name )->first()->id;
             $cp->category_id = $category_id;
             $cp->save();
-            return response()->json([
-                'Insert Data' => 'Successfully Inserted !',
-            ]);
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            return response()->json(['error' => $e->getMessage() . ' ' . $e->getLine()]);
-        }
-    }
-    public function delete($id)
-    {
-        try {
-
-            DB::table('category_product')
-                ->where('category_id', $id)
-                ->delete();
-
-            Category::find($id)
-                ->delete();
-
-            return response()->json([
-                'Delete Message' => 'Successfully Deleted !',
-            ]);
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            return response()->json(['error' => $e->getMessage() . ' ' . $e->getLine()]);
-        }
-    }
-    public function search(Request $request)
-    {
-        $query = $request->input('query');
-        $data = Category::join('category_product', 'category.id', '=', 'category_product.category_id')
-            ->join('product', 'category_product.product_id', '=', 'product.id')
-            ->where('category_product.category_id', $query)
-            ->orWhere('category.name', 'like', '%' . $query . '%')
-            ->orWhere('category.position', $query)
-            ->select(
-                'category_product.category_id',
-                'category.name',
-                'category.visible_in_menu',
-                'category.position',
-                'category.display_mode',
-                'category.decription',
-                'category.image',
-                'category.category_logo',
-                'category.parent_category',
-                'category.attributes',
-                'category.meta_title',
-                'category.slug',
-                'category.meta_description',
-                'category.meta_keyword',
-                'category.status',
-                'category.created_at',
-                'category.updated_at',
-                'product.product_type',
-
-            )->orderBy( 'category_product.category_id' )
-            ->paginate( 10 );
             return response()->json( [
-                'category' => $category,
+                'Insert Data' => 'Successfully Inserted !',
             ] );
         } catch ( Exception $e ) {
             Log::error( $e->getMessage() );
             return response()->json( [ 'error' => $e->getMessage() . ' ' . $e->getLine() ] );
         }
     }
-    public function show_data($id)
-    {
-        $category = Category::where('id', $id)->first();
 
-        return response()->json([
+    public function delete( $id ) {
+        try {
+
+            DB::table( 'category_product' )
+            ->where( 'category_id', $id )
+            ->delete();
+
+            Category::find( $id )
+            ->delete();
+
+            return response()->json( [
+                'Delete Message' => 'Successfully Deleted !',
+            ] );
+        } catch ( Exception $e ) {
+            Log::error( $e->getMessage() );
+            return response()->json( [ 'error' => $e->getMessage() . ' ' . $e->getLine() ] );
+        }
+    }
+
+    public function search( Request $request ) {
+        $query = $request->input( 'query' );
+        $data = Category::join( 'category_product', 'category.id', '=', 'category_product.category_id' )
+        ->join( 'product', 'category_product.product_id', '=', 'product.id' )
+        ->where( 'category_product.category_id', $query )
+        ->orWhere( 'category.name', 'like', '%' . $query . '%' )
+        ->orWhere( 'category.position', $query )
+        ->select(
+            'category.id',
+            'category.name',
+            'category.visible_in_menu',
+            'category.position',
+            'category.display_mode',
+            'category.decription',
+            'category.image',
+            'category.category_logo',
+            'category.parent_category',
+            'category.attributes',
+            'category.meta_title',
+            'category.slug',
+            'category.meta_description',
+            'category.meta_keyword',
+            'category.status',
+            'category.created_at',
+            'category.updated_at',
+            'category_product.product_id',
+            'product.quantity',
+            'product.created_at',
+            'product.updated_at',
+        )
+        ->paginate( 10 );
+
+        return response()->json( [
+            'Category' => $data,
+        ] );
+    }
+
+    public function show_data( $id ) {
+        $category = Category::where( 'id', $id )->first();
+
+        return response()->json( [
             'Show_Data' => $category,
-        ]);
+        ] );
     }
 }
