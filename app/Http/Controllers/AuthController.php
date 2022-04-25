4eda7e87902a710363e9865557330faf39f8bf31
Raval Arth <97344584+Raval-Arth@users.noreply.github.com>
2022-04-25 18:46:28 +0530

    In catch Response changed to HttpResponse

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Response;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use PHPUnit\Util\Json;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
@@ -25,11 +27,11 @@ class AuthController extends Controller
             if (!$token = auth('api')->attempt($credentials)) {
                 return response()->json([
                     'error' => 'Invalid Credentials',
-                ], Response::HTTP_INTERNAL_SERVER_ERROR);
+                ], HttpResponse::HTTP_INTERNAL_SERVER_ERROR);
             }
         } catch (JWTException $e) {
             Log::error($e->getMessage());
-            return response()->json(['error' => $e->getMessage() . ' ' . $e->getLine()], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return response()->json(['error' => $e->getMessage() . ' ' . $e->getLine()], HttpResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
         return response()->json([
             'access_token' => $token,
@@ -57,7 +59,7 @@ class AuthController extends Controller
         } catch (JWTException $exception) {
             return response()->json([
                 'message' => 'Sorry, user cannot be logged out'
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+            ], HttpResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
