aaf715611dab84a0f0e36c8439bb21bf86c9fc8d
tishya <tishya@vistaran.com>
2022-11-30 14:06:24 +0530

    - Category wise products listing

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $table = 'category';
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
